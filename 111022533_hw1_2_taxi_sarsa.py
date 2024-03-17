697e42b288485441fe03b98acc8dc19a0638f4cd
Ri-chard-Wu <glotigorgeous@gmail.com>
2024-03-17 10:55:27 +0800

    hw1_2_taxi_sarsa accu reward > 0

--- 111022533_hw1_2_taxi_sarsa.py
+++ 111022533_hw1_2_taxi_sarsa.py
@@ -1,5 +1,5 @@
 import gymnasium as gym
-
+import numpy as np
 # # import gym
 # env = gym.make("LunarLander-v2", render_mode="rgb_array")
 # env.action_space.seed(42)
@@ -13,91 +13,58 @@ import gymnasium as gym
 #         observation, info = env.reset()
 
 # env.close()
- 
-
+MIN_EXPLORING_RATE = 0.01
+MAX_EXPLORING_RATE = 0.5
+MIN_LEARNING_RATE = 0.5
+MAX_LEARNING_RATE = 0.5
 
 # https://www.gymlibrary.dev/environments/toy_text/taxi/
 env = gym.make('Taxi-v3') 
 env.action_space.seed(42)
 
-observation, info = env.reset(seed=42)
+
 
  
 
 class Agent:
 
-    def __init__(self,
-                 bucket_range_per_feature,
-                 num_action,
+    def __init__(self, 
                  t=0,
                  discount_factor=0.99):
+
         self.update_parameters(t)  # init explore rate and learning rate
-        self.q_table = {}
+        
         self.discount_factor = discount_factor
-        self.num_action = num_action
+        self.nA = 6
+        self.nS = 500
+        self.q_table = np.zeros((self.nS, self.nA))
 
     
-    def select_action(self, state):
-        # epsilon-greedy
-        state_idx = self.get_state_idx(state)
+    def select_action(self, state): 
         if np.random.rand() < self.exploring_rate:
-            action = np.random.choice(num_action)  # Select a random action
+            action = np.random.choice(self.nA)  # Select a random action
         else:
-            action = np.argmax(self.q_table[state_idx])  # Select the action with the highest q
+            action = np.argmax(self.q_table[state])  # Select the action with the highest q
         return action
 
-    def update_policy(self, state, action, reward, state_prime):
-        state_idx = self.get_state_idx(state)
-        state_prime_idx = self.get_state_idx(state_prime)
-        # Update Q_value using Q-learning update rule
-        best_q = np.max(self.q_table[state_prime_idx])
-        self.q_table[state_idx][action] += self.learning_rate * (
-            reward + self.discount_factor * best_q - self.q_table[state_idx][action])
-
-    def get_state_idx(self, state):
-        # instead of using absolute position of pipe, use relative position
-        state = copy.deepcopy(state)
-        state['next_next_pipe_bottom_y'] -= state['player_y']
-        state['next_next_pipe_top_y'] -= state['player_y']
-        state['next_pipe_bottom_y'] -= state['player_y']
-        state['next_pipe_top_y'] -= state['player_y']
-
-        # sort to make list converted from dict ordered in alphabet order
-        state_key = [k for k, v in sorted(state.items())]
-
-        # do bucketing to decrease state space to speed up training
-        state_idx = []
-        for key in state_key:
-            state_idx.append(
-                int(state[key] / self.bucket_range_per_feature[key]))
-        return tuple(state_idx)
+    def update_policy(self, state, action, reward, state_prime): 
+        best_q = np.max(self.q_table[state_prime])
+        self.q_table[state][action] += self.learning_rate * (
+            reward + self.discount_factor * best_q - self.q_table[state][action])
 
     def update_parameters(self, episode):
-        self.exploring_rate = max(MIN_EXPLORING_RATE,
-                                  min(0.5, 0.99**((episode) / 30)))
-        self.learning_rate = max(MIN_LEARNING_RATE, min(0.5, 0.99
-                                                        ** ((episode) / 30)))
+        self.exploring_rate = \
+                max(MIN_EXPLORING_RATE, min(MAX_EXPLORING_RATE, 0.99**((episode) / 30)))
+        self.learning_rate = \
+                max(MIN_LEARNING_RATE, min(MAX_LEARNING_RATE, 0.99 ** ((episode) / 30)))
 
     def shutdown_explore(self):
         # make action selection greedy
         self.exploring_rate = 0
 
 
-
-
-num_action = len(env.getActionSet())
-bucket_range_per_feature = {
-  'next_next_pipe_bottom_y': 40,
-  'next_next_pipe_dist_to_player': 512,
-  'next_next_pipe_top_y': 40,
-  'next_pipe_bottom_y': 20,
-  'next_pipe_dist_to_player': 20,
-  'next_pipe_top_y': 20,
-  'player_vel': 4,
-  'player_y': 16
-}
-# init agent
-agent = Agent(bucket_range_per_feature, num_action)
+ 
+agent = Agent()
  
  
 reward_per_epoch = []
@@ -108,48 +75,28 @@ print_every_episode = 500
 show_gif_every_episode = 5000
 NUM_EPISODE = 40000
 for episode in range(0, NUM_EPISODE):
-
-    # Reset the environment
-    env.reset_game()
-
-    # record frame
-    frames = [env.getScreenRGB()]
+ 
+    observation, info = env.reset(seed=42) 
 
     # for every 500 episodes, shutdown exploration to see performance of greedy action
     if episode % print_every_episode == 0:
         agent.shutdown_explore()
-
-    # the initial state
-    state = game.getGameState()
-    # cumulate reward for this episode
+ 
     cum_reward = 0  
     t = 0
 
-    while not env.game_over():
-
-        # select an action
-        action = agent.select_action(state)
-
-        # execute the action and get reward
-        # reward = +1 when pass a pipe, -5 when die
-        reward = env.act(env.getActionSet()[action])  
-
-        frames.append(env.getScreenRGB())
-
-        # cumulate reward
+    while(1): 
+        action = agent.select_action(observation) 
+        observation_next, reward, terminated, truncated, info = env.step(action) 
         cum_reward += reward
-
-        # observe the result
-        state_prime = game.getGameState()  # get next state
-
-        # update agent
-        agent.update_policy(state, action, reward, state_prime)
-
-        # Setting up for the next iteration
-        state = state_prime
+ 
+        agent.update_policy(observation, action, reward, observation_next)
+ 
+        observation = observation_next
         t += 1
 
-    # update exploring_rate and learning_rate
+        if terminated or truncated: break
+ 
     agent.update_parameters(episode)
 
     if episode % print_every_episode == 0:
@@ -172,6 +119,7 @@ for episode in range(0, NUM_EPISODE):
     #     display(clip.ipython_display(fps=60, autoplay=1, loop=1))
 
 
+print(agent.q_table)
 
 # """
 # print([i for i in env.unwrapped.decode(observation)]):
